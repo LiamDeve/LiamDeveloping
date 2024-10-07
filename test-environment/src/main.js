cf2467ebe53cceb75d9950322c15f8d49d39a861
Liam <jackh0517@gmail.com>
2024-10-07 17:07:13 +0800

    VITE更新

---
+++ test-environment/src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { Quasar } from 'quasar'; 
+import 'quasar/dist/quasar.css';
+
+const app = createApp(App);
+
+app.use(Quasar);
+
+app.mount('#app');
